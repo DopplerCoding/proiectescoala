e910e083a2ca011c7f4ed922a54f3176402ce1ba
DopplerCoding <44755069+DopplerCoding@users.noreply.github.com>
2018-11-12 19:12:26 +0200

    Add files via upload

---
+++ cmmdc6.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"a = ",cin>> a;
+
+    int b;
+    cout<<"b = ",cin>>b;
+
+    int rest;
+
+    rest = 1;
+
+    while(rest != 0) /// algortimul lui euclid
+    {
+        rest = a%b;
+        if(rest == 0)
+        {
+            //cout<<b;
+            break;
+        }
+        a = b;
+        b = rest;
+
+    }
+    if(b == 1)
+    {
+        cout<<"sunt prime intre ele";
+    }
+    if(b != 1)
+    {
+        cout<<"nu sunt prime intre ele";
+    }
+
+
+
+    return 0;
+}
